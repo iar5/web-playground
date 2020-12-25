2a7477c9d968cf69f56afd803a503732b28eb459
Tom Wendland <32841473+iar5@users.noreply.github.com>
2020-12-25 01:27:20 +0100

    add sticker, fix 3dcss

--- src/3dcss/3dcss.js
+++ src/3dcss/3dcss.js
@@ -1,66 +1,70 @@
 import * as THREE from '../../lib/three/build/three.module.js'
 import { OrbitControls } from '../../lib/three/examples/jsm/controls/OrbitControls.js'
-import { CSS3DRenderer } from '../../lib/three/examples/jsm/renderers/CSS3DRenderer.js.js'
+import { CSS3DRenderer, CSS3DObject } from '../../lib/three/examples/jsm/renderers/CSS3DRenderer.js'
 
 /**
  * Idee
-canvas über html und transparent an entsprechenden stellen
-canvas pointer events weiter geben an html mit css 
+ * canvas über html legen und an entsprechenden stellen transparent sein
+ * wenn etwa html text bearbeoten: canvas pointer events durchlassen
  */
 
 
-
-var renderer = new CSS3DRenderer({antialias: true})
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFShadowMap
-document.getElementById("webgl").appendChild(renderer.domElement)
+renderer.setClearColor(0x000000, 0)
+document.getElementById("html3D").appendChild(renderer.domElement)
 
-var camera = new THREE.PerspectiveCamera(55, window.innerWidth/window.innerHeight, 0.001, 10000)
+const cssRenderer = new CSS3DRenderer({antialias: true})
+cssRenderer.setSize(window.innerWidth, window.innerHeight)
+document.getElementById("webgl").appendChild(cssRenderer.domElement)
+
+
+const camera = new THREE.PerspectiveCamera(55, window.innerWidth/window.innerHeight, 0.001, 10000)
 camera.position.z = 3
-camera.position.y = 1
 
-var controls = new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
 controls.maxPolarAngle = THREE.MathUtils.degToRad(90)
 controls.minDistance = 0.1
 controls.maxDistance = 100
 controls.target.set(0, 0.5, 0)
 controls.update()
 
-var scene = new THREE.Scene()
+const scene = new THREE.Scene()
 
-var ambientLight = new THREE.AmbientLight('rgb(255,255,255)', 0.5) 
-scene.add(ambientLight)
+
 
       
-let htmlElement = document.getElementById("html3d")
+let htmlElement = document.getElementById("htmlSrc")
 htmlElement.style.display = "unset"
-let obj = new THREE.Object3D();
-let s = 0.008
-obj.scale.set(s, s, s) 
-tvGroup.add(obj)
 
-var css3dObject = new CSS3DObject(htmlElement);
-obj.add(css3dObject);
+let group = new THREE.Group();
+let s = 0.01
+group.scale.set(s, s, s) 
+scene.add(group)
 
-// chop plane
-var material = new THREE.MeshPhongMaterial({opacity: 0, color: new THREE.Color("black"), blending: THREE.NoBlending, side: THREE.DoubleSide });
-var geometry = new THREE.BoxBufferGeometry(htmlElement.clientWidth,  htmlElement.clientHeight, 0.1);
-var mesh = new THREE.Mesh(geometry, material);
-obj.add(mesh);
+// html
+let css3dObject = new CSS3DObject(htmlElement);
+group.add(css3dObject);
 
-material = new THREE.MeshBasicMaterial({opacity: 1, color: new THREE.Color("black")});
+
+// canvas chop plane
+let material = new THREE.MeshPhongMaterial({opacity: 0, color: new THREE.Color("black"), blending: THREE.NoBlending, side: THREE.DoubleSide });
+let geometry = new THREE.BoxBufferGeometry(htmlElement.clientWidth,  htmlElement.clientHeight, 0.1);
+let mesh = new THREE.Mesh(geometry, material);
+group.add(mesh);
+
+// back plate
+material = new THREE.MeshBasicMaterial({opacity: 1, color: new THREE.Color("red")});
 geometry = new THREE.BoxBufferGeometry(htmlElement.clientWidth+2,  htmlElement.clientHeight+2, 0.1);
 mesh = new THREE.Mesh(geometry, material);
-mesh.position.z = -2
-obj.add(mesh);
-
-material = new THREE.MeshBasicMaterial({opacity: 1, color: new THREE.Color("white"), wireframe: true});
-geometry = new THREE.BoxBufferGeometry(htmlElement.clientWidth-2,  htmlElement.clientHeight-2, 0.1);
-mesh = new THREE.Mesh(geometry, material);
 mesh.position.z = -1
-obj.add(mesh);
+group.add(mesh);
 
+// sphere davor
+material = new THREE.MeshBasicMaterial({opacity: 1, color: new THREE.Color("green")});
+geometry = new THREE.SphereBufferGeometry(htmlElement.clientWidth/4);
+mesh = new THREE.Mesh(geometry, material);
+group.add(mesh);
 
 
 
@@ -69,5 +73,6 @@ update()
 function update() {
     requestAnimationFrame(update)
     renderer.render(scene, camera)    
+    cssRenderer.render(scene, camera);
 }
 
